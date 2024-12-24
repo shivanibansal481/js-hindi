4760bd54cf3001568835603a4a02ef1af3bbf8a9
shivanibansal481 <114145659+shivanibansal481@users.noreply.github.com>
2024-12-24 14:01:07 +0000

    let var and const tut

---
+++ 01_basics/let_var_const.js
@@ -0,0 +1,17 @@
+const account_id=144553
+let account_email="shivani@gmail.com"
+var account_password="12345"
+account_city="panchkula"
+//account_id=2//not allowed
+console.log(account_id)
+account_email="hc@hmail"
+let account_state
+console.table([account_email,account_id,account_password,account_city,account_state])
+
+//var declare in two types var or let 
+/*diff is let use krkae us variable
+ a scope ni change ota yae problem 
+ thi pehle ki suppose let x agr vo for
+  mai use kra toh change hota tha but aaab nhi
+  but var use krkae usmae yae block scope ki problem aati hai
+  */
