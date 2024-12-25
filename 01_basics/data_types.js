ca430c5397c8c2f2cee8d0961fa857b9952ad62f
shivanibansal481 <114145659+shivanibansal481@users.noreply.github.com>
2024-12-25 06:49:49 +0000

    data types tutorial

---
+++ 01_basics/data_types.js
@@ -0,0 +1,13 @@
+"use strict"; //to tell js ka new vrsion hai yae 
+//alert(3+3)// not llowed s we re using nodejs not the browser in browser lert cuse  pop up
+// make your code redble 
+let name="shivani"//string
+let age =26 //int
+let loggedin = false //boolean
+// null is standalone value (it tells var is empty)
+//undefined (tells value is not assigned)
+//symbol (to tell uniqueness)
+//object
+
+console.log(typeof null) //tells that null is an object 
+console.log(typeof undefined)
