406f1f8ae921d49041b31a6100a51db7fe091b7a
shivanibansal481 <114145659+shivanibansal481@users.noreply.github.com>
2024-12-25 07:12:50 +0000

    conversion

---
+++ 01_basics/conversions.js
@@ -0,0 +1,36 @@
+// // let score = "55"
+// // console.log(typeof score)  // one way of writing type of 
+// // console.log(typeof (score)) // other way of writing is in form of method 
+
+// // let Value = Number(score) //converts score to  number 
+
+// // score ="777abc"            // number with alphbets 
+// // console.log(typeof score)  
+// //  Value = Number(score)
+// //  console.log(typeof (Value ))   // type would come Nan when converted to numner 
+// //  console.log(typeof NaN)    
+// //  /*  ** we put value as null it will convert to   0
+// //     ** if undefined Nan 
+// //     **if boolean than 1 or 0
+// //     ** if string than nan*/
+
+// // Value= "3+5"
+// // Value = Number(score)
+// // console.log(typeof (Value ))
+// // console.log(Value)
+
+// //to  boolean conversions 
+
+// let loggedIn =""
+// let value= Boolean(loggedIn) // returns false as no value is there 
+// console.log(value)
+
+// loggedIn ="shivani"
+//  value= Boolean(loggedIn) // returns false as no value is there 
+// console.log(value)
+
+
+// to string conversions 
+let loggedIn =""
+let value= String(loggedIn) // returns false as no value is there 
+console.log(value)
