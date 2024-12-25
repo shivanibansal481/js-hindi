dff59b38894afea12a9a1824a1f8f7475972ed20
shivanibansal481 <114145659+shivanibansal481@users.noreply.github.com>
2024-12-25 07:41:53 +0000

    operations on data types

---
+++ 01_basics/operations.js
@@ -0,0 +1,27 @@
+let value = 3
+let negvalue =-value
+console.log(negvalue)
+
+/******************string operations ***************** */
+let str1 ="shivani"
+let str2= "mohak"
+console.log(str1+str2)
+
+/******** very important************ */
+console.log("1"+2)
+console.log(1+"2")
+console.log(1+"2"+2) // output 122
+console.log("1"+2+2)  // output 122
+console.log(1+2+"2")  //output 32
+
+console.log(+true)
+console.log(+" ")
+
+/********************comparisons***************************** */
+console.log("2">888)
+console.log(undefined==null)
+
+/*********{===} check the datatype as well as value ******** */
+
+// primitive mai chnage in data type is done in copy (string,number,boolean,null,undefined etc 7 hote hai yae )
+//non primitive reference mai reference (array,objects)
